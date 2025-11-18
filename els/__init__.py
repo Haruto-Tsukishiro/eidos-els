42f2d63b2e7ca25fdc233039a19cc017f7ab627b
Haruto-Tsukishiro <verweile666nk@gmail.com>
2025-11-18 13:55:21 +0900

    Expose public ELS API
    Add module docstring and public interface for ELS.

--- els/__init__.py
+++ els/__init__.py
@@ -1,1 +1,16 @@
+"""
+ELS: Emotion Layer System (Demo)
 
+Public package interface.
+"""
+
+from .core import ELSCore, ELSConfig
+from .canonical import EmotionCanonical, SafetyLevel, SafetyGateResult
+
+__all__ = [
+    "ELSCore",
+    "ELSConfig",
+    "EmotionCanonical",
+    "SafetyLevel",
+    "SafetyGateResult",
+]
